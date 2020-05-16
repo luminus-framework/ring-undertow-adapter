7ca0774b089b9ca3324cb10bcce91976e6762e29
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2020-05-15 20:45:06 -0400

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject luminus/ring-undertow-adapter "1.0.5"
+(defproject luminus/ring-undertow-adapter "1.0.6"
   :description "Ring Underow adapter"
   :url "http://github.com/luminus-framework/ring-adapter-undertow"
   :license {:name "ISC License"
             :url  "http://opensource.org/licenses/ISC"}
-  :dependencies [[io.undertow/undertow-core "2.0.30.Final"]
-                 [ring/ring-core "1.8.0"]]
+  :dependencies [[io.undertow/undertow-core "2.1.1.Final"]
+                 [ring/ring-core "1.8.1"]]
   :profiles {:dev     {:dependencies [[org.clojure/clojure "1.10.1"]
-                                      [clj-http "3.10.0"]
+                                      [clj-http "3.10.1"]
                                       [stylefruits/gniazdo "1.1.3"]
                                       [metosin/reitit-ring "0.4.2"]]}
              :precomp {:prep-tasks ["compile"]}}

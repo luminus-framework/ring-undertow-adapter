88426e1fb10a0241b7be9d6ca9e606ac87be3d80
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-12-07 20:48:02 -0500

    bumped up version, test dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/ring-undertow-adapter "1.2.4"
+(defproject luminus/ring-undertow-adapter "1.2.5"
   :description "Ring Undertow adapter"
   :url "http://github.com/luminus-framework/ring-adapter-undertow"
   :license {:name "MIT License"
@@ -6,10 +6,10 @@
   :dependencies [[io.undertow/undertow-core "2.2.14.Final"]
                  [ring/ring-core "1.9.4"]]
   :profiles {:dev     {:dependencies [[org.clojure/clojure "1.10.3"]
-                                      [clj-http "3.10.1"]
-                                      [stylefruits/gniazdo "1.1.3"]
-                                      [metosin/reitit-ring "0.4.2"]
-                                      [criterium "0.4.5"]]
+                                      [clj-http "3.12.3"]
+                                      [stylefruits/gniazdo "1.2.0"]
+                                      [metosin/reitit-ring "0.5.15"]
+                                      [criterium "0.4.6"]]
                        :source-paths ["dev"]}
              :precomp {:prep-tasks ["clean" "compile"]}}
   :javac-options ["-target" "1.8" "-source" "1.8"]

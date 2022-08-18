a8588e547bb628734d6bb783bd21d1cca2c63bf4
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-08-18 09:34:21 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/ring-undertow-adapter "1.2.6"
+(defproject luminus/ring-undertow-adapter "1.2.7"
   :description "Ring Undertow adapter"
   :url "http://github.com/luminus-framework/ring-adapter-undertow"
   :license {:name "MIT License"
